d1d4ee5b503cd680d6f20b0035e4abdef04117b8
Sejal Dua <sejal.dua@tufts.edu>
2020-07-11 18:41:46 -0700

    working form to DB with chelsea's form

--- Intellivo-app/forms.py
+++ Intellivo-app/forms.py
@@ -30,25 +30,24 @@ class ProfileForm(FlaskForm):
 							validators=[DataRequired(), Length(min=2, max=40)], 
 							description=u"(First name only")
 	age = SelectField(u"Your Age: ", [DataRequired()],
-            choices=[("0", ""), ("1", "Under 18"), ("2", "18-25"), ("3", "26-40"), ("4", "40+")],
+            choices=[("1", "Under 18"), ("2", "18-25"), ("3", "26-40"), ("4", "40+")],
             description=u"(Select an age range.)",
             render_kw={})
 	spirituality = SelectField(u"How would you describe your belief in a higher power?", [DataRequired()],
-            choices=[("0", ""), ("0", "atheist"),("py", "agnostic"), ("rb", "spiritual but not religious"), ("js", "spiritual but also religious"),
-            		("0", "devout religious")],
+            choices=[("1", "atheist"),("2", "agnostic"), ("3", "spiritual but not religious"), ("4", "spiritual but also religious"),
+            		("5", "devout religious")],
             description=u"(How spiritual are you?)",
             render_kw={})
-	location = SelectMultipleField(u"Location: ", [],
-            choices=[("rb", "West Coast (US)"), ("js", "Mountain Region (US)"), ("js", "Midwest (US)"), ("js", "East Coast (US)"), ("py", "Canada"), 
-            		("js", "South America"),("js", "Mexico"), ("js", "East Coast"), ("js", "Europe"), ("js", "Africa"), 
-            		("js", "Western Asia"), ("js", "Eastern Asia"), ("js", "Southern Asia"),("js", "Australia"),("js", "Other"),],
-            description=u"(Your general place of residence)",
-            render_kw={"multiple": "multiple"})
+	location = SelectField(u"Location: ", [],
+            choices=[("1", "West Coast (US)"), ("2", "Mountain Region (US)"), ("3", "Midwest (US)"), ("4", "East Coast (US)"), ("5", "Canada"), 
+            		("6", "South America"),("7", "Mexico"), ("8", "East Coast"), ("9", "Europe"), ("10", "Africa"), 
+            		("11", "Western Asia"), ("12", "Eastern Asia"), ("13", "Southern Asia"),("14", "Australia"),("15", "Other"),],
+            description=u"(Your general place of residence)")
 	engagement = SelectField(u"When newsworthy things are happening in the world, at what level do you engage?", [DataRequired()],
-            choices=[("0", ""), ("py", "I do not engage"), ("rb", "I educate myself to understand the gist of it"), 
-            		("js", "In addition to educating myself, I initiate hard conversations with family and friends"),
-            		("py", "I participate in community-wide events surrounding social justice issues"),
-            		("py", "I am a full on activist: I organize events, create social media content, etc.")],
+            choices=[("1", "I do not engage"), ("2", "I educate myself to understand the gist of it"), 
+            		("3", "In addition to educating myself, I initiate hard conversations with family and friends"),
+            		("4", "I participate in community-wide events surrounding social justice issues"),
+            		("5", "I am a full on activist: I organize events, create social media content, etc.")],
             description=u"(Your engagement to events around you.)",
             render_kw={})
 
--- Intellivo-app/main.py
+++ Intellivo-app/main.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, url_for, flash, redirect# import the Flask class
+from flask import Flask, render_template, request, url_for, flash, redirect# import the Flask class
 from flask_sqlalchemy import SQLAlchemy #orm to run queries 
 from forms import RegistrationForm, LoginForm, ProfileForm
 
+import os
+
 app = Flask(__name__) # set app variable to an instance of the flask class
 
-#create secret key
+# create secret key
 app.config['SECRET_KEY']='a27adadd2e3e4bb099e737cd7c3257e4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # chose your database
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # class User(db.Model):
 # 	id = db.Column(db.Integer, primary_key=True)
@@ -23,52 +25,70 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # chose your databas
 
 # db for chat preference form 
 # class ChatPreferences(db.Model):
-	# preferences = db.relationship('Preference', backref='author') # lazy=dynamic?
-	# user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	# include columns for actual preferences from form here 
+    # preferences = db.relationship('Preference', backref='author') # lazy=dynamic?
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # include columns for actual preferences from form here 
+
+def init_db():
+    db.create_all()
+
+class User(db.Model):
+    first_name = db.Column(db.String(80), unique=False, nullable=False, primary_key=True)
+    age = db.Column(db.Integer, unique=False, nullable=False, primary_key=True)
+    spirituality = db.Column(db.Integer, unique=False, nullable=False, primary_key=True)
+    location = db.Column(db.Integer, unique=False, nullable=False, primary_key=True)
+    engagement = db.Column(db.Integer, unique=False, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<First Name: {} Age: {} Spirituality: {} Location: {} Engagement: {}>".format(self.first_name, self.age, self.spirituality, self.location, self.engagement)
 
 
 @app.route("/") # home page 
 @app.route("/home")
 def home():
-	return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home')
 
 
 
 @app.route("/about") # home page 
 def about():
-	return render_template('about.html', title='About')
+    return render_template('about.html', title='About')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
- 	form = RegistrationForm()
- 	if form.validate_on_submit():
- 		flash(f'Account created for {form.username.data}!', 'success')
- 		return redirect(url_for('home'))
- 	return render_template('register.html', title='Register', form=form)
-	
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+    
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-	form=LoginForm()
-	if form.validate_on_submit():
-			if form.email.data == 'admin@log.com' and form.password.data == 'password':
-				flash('You have been logged in!', 'success')
-				return redirect(url_for('user')) # was 'home'
-			else:
-				flash('Login unsucessful. please check credentials', 'danger')
-	return render_template('login.html', title='Login', form=form)
+    form=LoginForm()
+    if form.validate_on_submit():
+            if form.email.data == 'admin@log.com' and form.password.data == 'password':
+                flash('You have been logged in!', 'success')
+                return redirect(url_for('user')) # was 'home'
+            else:
+                flash('Login unsucessful. please check credentials', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/user")
 def user():
-	return render_template('userChats.html', title='User Home')
+    return render_template('userChats.html', title='User Home')
 
 @app.route("/preferences",  methods=['GET', 'POST'])
 def preferences():
-	form=ProfileForm()
-	if form.validate_on_submit():
-		return redirect(url_for('user')) # was 'home'
-	return render_template('form.html', title='Preferences', form=form)
+    form=ProfileForm()
+    if form.validate_on_submit():
+        print(form.firstname.data, form.age.data, form.spirituality.data, form.location.data, form.engagement.data)
+        user = User(first_name=form.firstname.data, age=int(form.age.data), spirituality=int(form.spirituality.data), location=int(form.location.data), engagement=int(form.engagement.data))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user')) # was 'home'
+    return render_template('form.html', title='Preferences', form=form)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    init_db()
+    app.run(debug=True)
 
